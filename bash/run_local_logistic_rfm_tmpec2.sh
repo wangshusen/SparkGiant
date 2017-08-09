14a89afe16629c86cd8b29e889e19b7a39b4f860
Shusen Wang <wssatzju@gmail.com>
2017-08-09 12:31:05 -0700

    minor changes

--- bash/run_local_logistic_rfm_tmpec2.sh
+++ bash/run_local_logistic_rfm_tmpec2.sh
@@ -2,13 +2,11 @@
 
 export PROJ_HOME="/root/SparkGiant"
 export JAR_FILE="$PROJ_HOME/target/scala-2.11/giant_2.11-1.0.jar"
-export DATA_FILE=$PROJ_HOME"/data/covtype"
+export DATA_FILE=$PROJ_HOME"/data/covtype_perm"
 export SPARK_HOME="/root/spark-2.1.0-bin-hadoop2.7"
 NUM_SPLITS="4"
 MASTER="local["$NUM_SPLITS"]"
 NUM_FEATURE="200"
-
-DATA_FILE="$PROJ_HOME/data/covtype_perm"
 
 $SPARK_HOME/bin/spark-submit \
     --class "distopt.logistic.ExperimentRfm" \
